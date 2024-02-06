96ff28160234505ab3f807a2b86578a8f4a8948f
Micah Acinapura <micahnyc@gmail.com>
2024-02-06 18:40:30 -0500

    wip for changing targets

--- session-recording-to-spice/rec2spice.py
+++ session-recording-to-spice/rec2spice.py
@@ -1,11 +1,17 @@
 #rec2spice.py
+import sys
 
+filename = sys.argv[1]
 # Using readlines()
-file = open('a174.osrectxt', 'r')
+file = open(filename, 'r')
 Lines = file.readlines()
- 
+
+
+SWITCHSTR = 'openspace.setPropertyValueSingle("NavigationHandler.OrbitalNavigator.Anchor'
+
 count = 0
 # Strips the newline character
+masterFrames = []
 frames = []
 for line in Lines:
     count += 1
@@ -16,30 +22,48 @@ for line in Lines:
         x =  components[4]
         y =  components[5]
         z =  components[6]
+        qx =  components[7]
+        qy =  components[8]
+        qz =  components[9]
+        qw =  components[10]
         focus = components[-1].strip()
         frames.append({'et':float(et), 'x': float(x), 'y': float(y), 'z': float(z), 'focus': focus})
+    elif (SWITCHSTR in line):
+        print("switch focus")
+        masterFrames.append(frames)
+        frames = []
 
-count = 0
-framecount = len(frames)
-for frame in frames:
-    if ( (count > 0) and (count < framecount-1) ):
-        dt = frames[count+1]['et'] - frames[count-1]['et']
-        dx = abs(frames[count+1]['x']) - abs(frames[count-1]['x'])
-        dy = abs(frames[count+1]['y']) - abs(frames[count-1]['y'])
-        dz = abs(frames[count+1]['z']) - abs(frames[count-1]['z'])
-        frame['vx'] = dx/dt
-        frame['vy'] = dy/dt
-        frame['vz'] = dz/dt
-    count += 1
-
-frames.pop(0)
-frames.pop(-1)
-
-with open('states.txt', 'w') as f:
+masterFrames.append(frames)
+for frames in masterFrames:
+    count = 0
+    framecount = len(frames)
+    print("frameCount" + str(framecount))
     for frame in frames:
-        print(frame)
-        line = "{}, {}, {}, {}, {}, {}, {}\n".format(
-            frame['et'], frame['x'], frame['y'], frame['z'], frame['vx'], frame['vy'], frame['vz'])
-        f.write(line)
-    f.close()
+        if ( (count > 0) and (count < framecount-1) ):
+            dt = frames[count+1]['et'] - frames[count-1]['et']
+            dx = abs(frames[count+1]['x']  - frames[count-1]['x'])
+            dy = abs(frames[count+1]['y'] - frames[count-1]['y'])
+            dz = abs(frames[count+1]['z'] - frames[count-1]['z'])
+            frame['vx'] = dx/dt
+            frame['vy'] = dy/dt
+            frame['vz'] = dz/dt
+        count += 1
+    frames.pop(0)
+    frames.pop(-1)
+
+print(masterFrames[0][0])
+print(masterFrames[1][0])
+print("frameCount|" + str(len(masterFrames[0])))
+print("frameCount|" + str(len(masterFrames[1])))
+
+
+
+for frames in masterFrames:
+    with open('states_' + frames[0]['focus'] + '.txt', 'w') as f:
+        for frame in frames:
+            #print(frame)
+            line = "{}, {}, {}, {}, {}, {}, {}\n".format(
+                frame['et'], frame['x'], frame['y'], frame['z'], frame['vx'], frame['vy'], frame['vz'])
+            f.write(line)
+        f.close()
 
