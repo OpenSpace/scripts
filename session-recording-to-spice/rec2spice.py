739b67cce87b56e28cce64fa5f46b88162d9a2ed
Micah Acinapura <micahnyc@gmail.com>
2024-02-06 20:16:25 -0500

    adding support for multiple targets

--- session-recording-to-spice/rec2spice.py
+++ session-recording-to-spice/rec2spice.py
@@ -1,45 +1,50 @@
 #rec2spice.py
-import sys
 
+#read the recording file into Lines array
+import sys
 filename = sys.argv[1]
-# Using readlines()
 file = open(filename, 'r')
 Lines = file.readlines()
 
-
+#string in file that represents switching targets
 SWITCHSTR = 'openspace.setPropertyValueSingle("NavigationHandler.OrbitalNavigator.Anchor'
 
+NAIF_CODES = {
+    'Moon': 301,
+    'Earth': 399,
+    'Mars': 499,
+}
+
+#loop thru lines of file to create 'frames' for each segment of the recording based on target
 count = 0
-# Strips the newline character
 masterFrames = []
 frames = []
 for line in Lines:
-    count += 1
     components = line.split(" ")
-
+    focus = ''
     if (components[0] == 'camera'):
-        et = components[3]
-        x =  components[4]
-        y =  components[5]
-        z =  components[6]
-        qx =  components[7]
-        qy =  components[8]
-        qz =  components[9]
-        qw =  components[10]
-        focus = components[-1].strip()
-        frames.append({'et':float(et), 'x': float(x), 'y': float(y), 'z': float(z), 'focus': focus})
+        frames.append({'et':float(components[3]),
+                        'x': float(components[4]),
+                        'y': float(components[5]),
+                        'z': float(components[6]),
+                        'i': float(components[7]),
+                        'j': float(components[8]),
+                        'k': float(components[9]),
+                        'l': float(components[10]),
+                        'focus': components[-1].strip()})
     elif (SWITCHSTR in line):
-        print("switch focus")
+        #focus is switched, add frames to master list and create new list
         masterFrames.append(frames)
         frames = []
+    count += 1
+masterFrames.append(frames) #add final list of frames to master list
 
-masterFrames.append(frames)
+#loop throu master list and add velocities 
 for frames in masterFrames:
     count = 0
     framecount = len(frames)
-    print("frameCount" + str(framecount))
     for frame in frames:
-        if ( (count > 0) and (count < framecount-1) ):
+        if ( (count > 0) and (count < framecount-1) ): #skip first and last frames when calc'ing central difference 
             dt = frames[count+1]['et'] - frames[count-1]['et']
             dx = abs(frames[count+1]['x']  - frames[count-1]['x'])
             dy = abs(frames[count+1]['y'] - frames[count-1]['y'])
@@ -48,22 +53,60 @@ for frames in masterFrames:
             frame['vy'] = dy/dt
             frame['vz'] = dz/dt
         count += 1
+    #ditch first and last frames with no velocity
     frames.pop(0)
     frames.pop(-1)
 
-print(masterFrames[0][0])
-print(masterFrames[1][0])
-print("frameCount|" + str(len(masterFrames[0])))
-print("frameCount|" + str(len(masterFrames[1])))
-
-
-
 for frames in masterFrames:
-    with open('states_' + frames[0]['focus'] + '.txt', 'w') as f:
+    focus = frames[0]['focus']
+    #create .dat _position files formatted for spice with one lines per frame
+    with open(focus + '_pos' + '.dat', 'w') as f:
         for frame in frames:
-            #print(frame)
             line = "{}, {}, {}, {}, {}, {}, {}\n".format(
                 frame['et'], frame['x'], frame['y'], frame['z'], frame['vx'], frame['vy'], frame['vz'])
             f.write(line)
         f.close()
+    #create .dat _orientation files formatted for spice with one lines per frame
+    with open(focus + '_ori' + '.dat', 'w') as f:
+        for frame in frames:
+            line = "{}, {}, {}, {}, {}\n".format(
+                frame['et'], frame['i'], frame['j'], frame['k'], frame['l'])
+            f.write(line)
+        f.close()
+    #generate setup.mkspk
+    center_id = NAIF_CODES[focus]
+    if (not center_id):
+        print("Existing due to error looking up naif id for " + focus)
+        exit(-1)
+    with open(focus+".mkspk", 'w') as f:
+        mkspk = """\\begindata\n\
+            INPUT_DATA_TYPE   = 'STATES'\n\
+            DATA_ORDER        = 'epoch x y z vx vy vz '\n\
+            DATA_DELIMITER    = ','\n\
+            LINES_PER_RECORD  = 1\n\
+            PRODUCER_ID       = 'OpenSpace (rec2spice)'\n\
+            OUTPUT_SPK_TYPE   = 9\n\
+            INPUT_DATA_UNITS  = ('DISTANCES=METERS')\n\
+            OBJECT_ID         = -20010024\n\
+            CENTER_ID         = %s\n\
+            REF_FRAME_NAME    = 'J2000'\n\
+            LEAPSECONDS_FILE  = 'naif0012.tls'\n\
+            INPUT_DATA_FILE   = '%s'\n\
+            OUTPUT_SPK_FILE   = '%s.bsp'\n\
+            COMMENT_FILE      = 'commnt.txt'\n\
+            POLYNOM_DEGREE    = 9\n\
+            TIME_WRAPPER      = '# ETSECONDS'\n\
+            APPEND_TO_OUTPUT  = 'YES'\n\
+        \\begintext\n""" % (str(center_id), focus + '_pos.dat', filename)
+        f.write(mkspk)
+        f.close()
+
+# with open(focus+".mkspk", 'w') as f:
+#         mkspk = """\\begindata\n\
+#             INPUT_DATA_TYPE   = '%s'\n\
+#             APPEND_TO_OUTPUT  = 'YES'\n\
+#         \\begintext\n""" % (str(center_id), focus + '_pos.dat', filename)
+#         f.write(mkspk)
+#         f.close()
+
 
