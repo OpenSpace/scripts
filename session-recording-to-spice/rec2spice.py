eea9777c6170edb5ce6b23e450632476d8781ccf
Micah Acinapura <micahnyc@gmail.com>
2024-02-07 20:11:02 -0500

    work on frames, need to have OS view trail in frame it was created in

--- session-recording-to-spice/rec2spice.py
+++ session-recording-to-spice/rec2spice.py
@@ -1,11 +1,25 @@
 #rec2spice.py
+import os
 import sys
 import textwrap
+import platform
+from astropy.time import TimeDelta, Time
+from astropy import units as u
+import spiceypy
 
 #read the recording file into Lines array
 filename = sys.argv[1]
+if (filename[0:2] == ".\\"):
+    filename = filename[2:]
 file = open(filename, 'r')
 Lines = file.readlines()
+
+newname = filename.split(".")[0]
+
+myfile = newname + ".bsp"
+# If file exists, delete it.
+if os.path.isfile(myfile):
+    os.remove(myfile)
 
 #string in file that represents switching targets
 SWITCHSTR = 'openspace.setPropertyValueSingle("NavigationHandler.OrbitalNavigator.Anchor'
@@ -45,6 +59,17 @@ masterFrames.append(frames) #add final list of frames to master list
 et_start = masterFrames[0][0]['et']
 et_end = masterFrames[-1][-1]['et']
 
+#astropy conver
+#iso_start = (Time(2000, format='jyear') + TimeDelta(et_start*u.s)).iso
+#iso_end = (Time(2000, format='jyear') + TimeDelta(et_end*u.s)).iso
+
+#spicepyconvert
+spiceypy.spiceypy.furnsh('naif0012.tls')
+iso_start = spiceypy.spiceypy.et2utc(et_start, 'ISOC', 8)
+iso_end = spiceypy.spiceypy.et2utc(et_end, 'ISOC', 8)
+
+print(f"start {iso_start} | end {iso_end}")
+
 #loop throu master list and add velocities 
 for frames in masterFrames:
     count = 0
@@ -52,9 +77,9 @@ for frames in masterFrames:
     for frame in frames:
         if ( (count > 0) and (count < framecount-1) ): #skip first and last frames when calc'ing central difference 
             dt = frames[count+1]['et'] - frames[count-1]['et']
-            dx = abs(frames[count+1]['x']  - frames[count-1]['x'])
-            dy = abs(frames[count+1]['y'] - frames[count-1]['y'])
-            dz = abs(frames[count+1]['z'] - frames[count-1]['z'])
+            dx = frames[count+1]['x']  - frames[count-1]['x']
+            dy = frames[count+1]['y'] - frames[count-1]['y']
+            dz = frames[count+1]['z'] - frames[count-1]['z']
             frame['vx'] = dx/dt
             frame['vy'] = dy/dt
             frame['vz'] = dz/dt
@@ -91,11 +116,11 @@ for frames in masterFrames:
             DATA_DELIMITER    = ','
             LINES_PER_RECORD  = 1
             PRODUCER_ID       = 'OpenSpace (rec2spice)'
-            OUTPUT_SPK_TYPE   = 12
+            OUTPUT_SPK_TYPE   = 13
             INPUT_DATA_UNITS  = ('DISTANCES=METERS')
-            OBJECT_ID         = %s
             CENTER_ID         = %s
-            REF_FRAME_NAME    = 'J2000'
+            OBJECT_ID         = %s
+            REF_FRAME_NAME    = 'GALACTIC'
             LEAPSECONDS_FILE  = 'naif0012.tls'
             INPUT_DATA_FILE   = '%s'
             OUTPUT_SPK_FILE   = '%s.bsp'
@@ -103,73 +128,84 @@ for frames in masterFrames:
             POLYNOM_DEGREE    = 9
             TIME_WRAPPER      = '# ETSECONDS'
             APPEND_TO_OUTPUT  = 'YES'
-        \\begintext\n""" % (str(center_id), SPICE_ID, focus + '_pos.dat', filename)
+        \\begintext\n""" % (str(center_id), SPICE_ID, focus + '_pos.dat', newname)
         textwrap.dedent(mkspk)
         f.write(mkspk)
         f.close()
+        os.system(f"mkspk.exe -setup {focus}.mkspk")
 
-    with open(filename+".asset", 'w') as f:
-        asset_str = """        local name = '%s'
-        local pos = {
-            Identifier = name .. '_pos',
-            Parent = 'Sun',
-            Transform = {
-                Translation = {
-                    Type = "SpiceTranslation",
-                    Target = "%s",
-                    Observer = 'SUN',
-                    Frame = "J2000"
+with open(newname+".asset", 'w') as f:
+    asset_str = """    
+    local sun = asset.require("scene/solarsystem/sun/sun")
+    local name = '%s'
+    local kernel = asset.localResource(name .. '.bsp')
+    local pos = {
+        Identifier = name .. '_pos',
+        Parent = 'Moon',
+        Transform = {
+            Translation = {
+                Type = "SpiceTranslation",
+                Target = "%s",
+                Observer = '301',
+                Frame = "IAU_MOON"
+            }
+        },
+        Renderable = {
+            Type="RenderableModel",
+            GeometryFile = asset.localResource('xwing.glb'),
+            LightSources = {
+                {
+                    Type = "CameraLightSource",
+                    Intensity = 0.6
                 }
-            },
-            GUI = {
-                Path = '/SS7',
-                Name = name .. " position"
             }
+        },
+        GUI = {
+            Path = '/SS7',
+            Name = name .. " position"
         }
+    }
+    """ % (newname, SPICE_ID)
 
-        local trail = {
-            Identifier = name .. '_trail',
-            Parent = "Sun",
-            Renderable = {
-                Type = "RenderableTrailTrajectory",
-                Translation = {
-                    Type = "SpiceTranslation",
-                    Target = "%s",
-                    Observer = 'SUN',
-                    Frame = "J2000"
-                },
-                Color = { 0.9, 0.9, 0.0 },
-                StartTime = %s,
-                EndTime = %s,
-                SampleInterval = 1,
-                TimeStampSubsampleFactor = 1,
-                EnableFade = false,
-                ShowFullTrail = true
+    asset_str += """local trail = {
+        Identifier = name .. '_trail',
+        Parent = "Moon",
+        Renderable = {
+            Type = "RenderableTrailTrajectory",
+            Translation = {
+                Type = "SpiceTranslation",
+                Target = "%s",
+                Observer = '301',
+                Frame = "IAU_MOON"
             },
-            GUI = {
-                Path = '/SS7',
-                Name = name .. " trail"
-            }
+            Color = { 0.9, 0.9, 0.0 },
+            StartTime = '%s',
+            EndTime = '%s',
+            SampleInterval = 1,
+            TimeStampSubsampleFactor = 1,
+            EnableFade = false,
+            ShowFullTrail = true
+        },
+        GUI = {
+            Path = '/SS7',
+            Name = name .. " trail"
         }
+    }
+    """ % (SPICE_ID, iso_start, iso_end)
 
-        local kernel = asset.localResource(name .. '.osrectxt.bsp')
+    asset_str += """
+    asset.onInitialize(function()
+        openspace.spice.loadKernel(kernel)
+        openspace.addSceneGraphNode(pos)
+        openspace.addSceneGraphNode(trail)
+    end)
 
-        asset.onInitialize(function()
-            openspace.spice.loadKernel(kernel)
-            openspace.addSceneGraphNode(pos)
-            openspace.addSceneGraphNode(trail)
-        end)
-
-        asset.onDeinitialize(function()
-            openspace.removeSceneGraphNode(pos)
-            openspace.removeSceneGraphNode(trail)
-            openspace.spice.unloadKernel(kernel)
-        end)
-
-
-        """ % (filename, SPICE_ID, SPICE_ID, et_start, et_end)
-        textwrap.dedent(asset_str)
-        f.write(asset_str)
-        f.close()
-
-
+    asset.onDeinitialize(function()
+        openspace.removeSceneGraphNode(pos)
+        openspace.removeSceneGraphNode(trail)
+        openspace.spice.unloadKernel(kernel)
+    end)
+    """
+    asset_str = textwrap.dedent(asset_str)
+    f.write(asset_str)
+    f.close()
